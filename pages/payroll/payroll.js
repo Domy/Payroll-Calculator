e2e7acffe1af20eefe23cf83b79743f60defdcb3
Domy <wkaifang@gmail.com>
2016-12-29 16:05:01 +0800

    init

---
+++ pages/payroll/payroll.js
@@ -0,0 +1,87 @@
+//payroll-calculator.js
+import {getTaxLevel} from '../../utils/util.js';
+
+Page({
+  data: {
+    index: 0,
+    cities: ['北京', '上海', '广州', '深圳'],
+    cityRate: {
+      '北京': {
+        base: 7086 * 3,
+        housefund: 12,
+        workfund: 0.2,
+        medfund: 2,
+        agefund: 8 
+      },
+      '上海': {
+        base: 5939 * 3,
+        housefund: 7,
+        workfund: 0.5,
+        medfund: 2,
+        agefund: 8
+      },
+      '深圳': {
+        base: 6054 * 3,
+        housefund: 13,
+        workfund: 0.5,
+        medfund: 2,
+        agefund: 8
+      },
+      '广州': {
+        base: 5525 * 3,
+        housefund: 10,
+        workfund: 0.5,
+        medfund: 2,
+        agefund: 8
+      }
+    },
+    income: 0,
+    results: {}
+  },
+  bindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindKeyInput: function(e) {
+    this.setData({
+      income: e.detail.value
+    })
+  },
+  generateResult: function(e) {
+    let city = this.data.cities[this.data.index];
+    let rate = this.data.cityRate[city];
+
+    let income = this.data.income;
+    let houseFund = Math.min(income, rate.base) * (rate.housefund / 100);
+    let workFund = Math.min(income, rate.base) * (rate.workfund / 100);
+    let medFund = Math.min(income, rate.base) * (rate.medfund / 100);
+    let ageFund = Math.min(income, rate.base) * (rate.agefund / 100);
+
+    let incomeBefore = income - houseFund - workFund - medFund - ageFund;
+    let taxLevel = getTaxLevel(incomeBefore - 3500)
+    let tax = (taxLevel.rate * (incomeBefore - 3500)) - taxLevel.quota;
+    let incomeTotal = incomeBefore - tax;
+
+    this.setData({
+      results: {
+        houseFund: houseFund.toFixed(2),
+        workFund: workFund.toFixed(2),
+        medFund: medFund.toFixed(2),
+        ageFund: ageFund.toFixed(2),
+        incomeTotal: incomeTotal.toFixed(2),
+        incomeBefore: incomeBefore.toFixed(2),
+        tax: tax.toFixed(2)
+      }
+    })
+  },
+  clear: function(e) {
+    this.setData({
+      results: {}
+    })
+    this.setData({
+      income: 0
+    })
+  }
+})
---
+++ utils/util.js
@@ -0,0 +1,41 @@
+function formatTime(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+function formatNumber(n) {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+function getTaxLevel (n) {
+  if (n <= 0) {
+    return { rate: 0, quota: 0};
+  } else if (n < 1500 ) {
+    return { rate: 0.03, quota: 0};
+  } else if (n < 4500) {
+    return { rate: 0.10, quota: 105};
+  } else if (n < 9000) {
+    return { rate: 0.20, quota: 555};
+  } else if (n < 35000) {
+    return { rate: 0.25, quota: 1005};
+  } else if (n < 55000) {
+    return { rate: 0.30, quota: 2755};
+  } else if (n < 80000) {
+    return { rate: 0.35, quota: 5505};
+  } else {
+    return { rate: 0.45, quota: 13505};
+  }
+}
+
+module.exports = {
+  formatTime: formatTime,
+  getTaxLevel: getTaxLevel
+}
