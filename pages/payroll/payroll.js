dd1aeb251d6a997f23af22e32ec84485779ef6ec
Domy <wkaifang@gmail.com>
2017-01-17 22:02:28 +0800

    update

--- pages/payroll/payroll.js
+++ pages/payroll/payroll.js
@@ -36,7 +36,29 @@ Page({
       }
     },
     income: 0,
-    results: {}
+    results: {},
+    paymentDetail: [
+      {
+        name: '养老保险',
+        rate: 0.08,
+        amount: 800
+      },
+      {
+        name: '医疗保险',
+        rate: 0.02,
+        amount: 200
+      },
+      {
+        name: '失业保险',
+        rate: 0.01,
+        amount: 100
+      },
+      {
+        name: '住房公积金',
+        rate: 0.12,
+        amount: 1200
+      }
+    ]
   },
   bindPickerChange: function(e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
