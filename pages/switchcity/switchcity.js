555601dd28d15db12edf3a1f8b44de05a4f4d4fb
qbatyqi <qbaty.qi@gmail.com>
2017-01-22 15:53:09 +0800

    fixed path

--- pages/switchcity/switchcity.js
+++ pages/switchcity/switchcity.js
@@ -70,7 +70,7 @@ Page({
     // 用户点击右上角分享
     return {
       title: '税后工资计算器', // 分享标题
-      desc: '税后工资，年终奖计算', // 分享描述
+      desc: '税后工资，年终奖计算', // 分享描述
       path: '/pages/payroll/payroll' // 分享路径
     }
   },
