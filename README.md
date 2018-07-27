8ab41c8e844f63d7d274e7eea48744886cbd1dff
王开放 <wangkaifang@baidu.com>
2018-07-27 14:40:07 +0800

    format

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Payroll-Calculator
 税后工资计算器
+
+基数 base max min
+医疗保险 25401 5080 medical
+养老保险 25401 3387 pension
+失业保险 25401 3387 unemployment
+生育保险 25401 5080 fertility
+工伤保险 25401 5080 workInjury
+住房公积金 25401 2000 housingFund
+
+比例 ratio company personal
+医疗保险 10% 5%
+养老保险 19% 8%
+失业保险 0.8% 0
+生育保险 0.8% 0
+工伤保险 0.3% 0
+住房公积金 12% 12%
+
+socialInsurance 社会保险
+housingFund 住房公积金
+
+beforeTax  税前
+afterTax  税后
--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 import { on } from 'utils/event.js'
 
 App({
-  onLaunch: function() {
-    //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    onLaunch: function () {
+        //调用API从本地缓存中获取数据
+        var logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs)
 
-    on('changeCity', this, (data) => {
-      this.globalData.city = data
-    })
-  },
-  onShow: function() {
-    console.log('App Show')
-  },
-  onHide: function() {
-    console.log('App Hide')
-  },
-  getUserInfo: function(cb) {
-    var that = this
-    if (this.globalData.userInfo) {
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    } else {
-      //调用登录接口
-      wx.login({
-        success: function() {
-          wx.getUserInfo({
-            success: function(res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
+        on('changeCity', this, (data) => {
+            this.globalData.city = data
+        })
+    },
+    onShow: function () {
+        console.log('App Show')
+    },
+    onHide: function () {
+        console.log('App Hide')
+    },
+    getUserInfo: function (cb) {
+        var that = this
+        if (this.globalData.userInfo) {
+            typeof cb == "function" && cb(this.globalData.userInfo)
+        } else {
+            //调用登录接口
+            wx.login({
+                success: function () {
+                    wx.getUserInfo({
+                        success: function (res) {
+                            that.globalData.userInfo = res.userInfo
+                            typeof cb == "function" && cb(that.globalData.userInfo)
+                        }
+                    })
+                }
+            })
         }
-      })
+    },
+    globalData: {
+        userInfo: null,
+        city: '北京市'
     }
-  },
-  globalData: {
-    userInfo: null,
-    city: '北京市'
-  }
 })
